c7bcaf14b838a04c01d19aacc4e4c265062d5181
Jahangir Alom <jahangir.alom@portonics.com>
2023-07-18 15:51:41 +0600

    Multiscreen routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:lazyload/screens/blog.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -21,13 +20,22 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'PortPos LazyLoad Page'),
+      home: const MyHomePage(title: 'PortPos Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
@@ -36,102 +44,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // We will fetch data from this Rest api
-  final _baseUrl = 'https://jsonplaceholder.typicode.com/posts';
+  int _counter = 0;
 
-  // At the beginning, we fetch the first 20 posts
-  int _page = 0;
-
-  // you can change this value to fetch more or less posts per page (10, 15, 5, etc)
-  final int _limit = 20;
-
-  // There is next page or not
-  bool _hasNextPage = true;
-
-  // Used to display loading indicators when _firstLoad function is running
-  bool _isFirstLoadRunning = false;
-
-  // Used to display loading indicators when _loadMore function is running
-  bool _isLoadMoreRunning = false;
-
-  // This holds the posts fetched from the server
-  List _posts = [];
-
-  // This function will be called when the app launches (see the initState function)
-  void _firstLoad() async {
+  void _incrementCounter() {
     setState(() {
-      _isFirstLoadRunning = true;
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
-    try {
-      final res =
-      await http.get(Uri.parse("$_baseUrl?_page=$_page&_limit=$_limit"));
-      setState(() {
-        _posts = json.decode(res.body);
-      });
-    } catch (err) {
-      if (kDebugMode) {
-        print('Something went wrong');
-      }
-    }
-
-    setState(() {
-      _isFirstLoadRunning = false;
-    });
-  }
-
-  // This function will be triggered whenver the user scroll
-  // to near the bottom of the list view
-  void _loadMore() async {
-    if (_hasNextPage == true &&
-        _isFirstLoadRunning == false &&
-        _isLoadMoreRunning == false &&
-        _controller.position.extentAfter < 300) {
-      setState(() {
-        _isLoadMoreRunning = true; // Display a progress indicator at the bottom
-      });
-      _page += 1; // Increase _page by 1
-      try {
-        final res =
-        await http.get(Uri.parse("$_baseUrl?_page=$_page&_limit=$_limit"));
-
-        final List fetchedPosts = json.decode(res.body);
-        if (fetchedPosts.isNotEmpty) {
-          setState(() {
-            _posts.addAll(fetchedPosts);
-          });
-        } else {
-          // This means there is no more data
-          // and therefore, we will not send another GET request
-          setState(() {
-            _hasNextPage = false;
-          });
-        }
-      } catch (err) {
-        if (kDebugMode) {
-          print('Something went wrong!');
-        }
-      }
-
-      setState(() {
-        _isLoadMoreRunning = false;
-      });
-    }
-  }
-
-  // The controller for the ListView
-  late ScrollController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _firstLoad();
-    _controller = ScrollController()..addListener(_loadMore);
-  }
-
-  @override
-  void dispose() {
-    _controller.removeListener(_loadMore);
-    super.dispose();
   }
 
   @override
@@ -152,47 +75,34 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: _isFirstLoadRunning
-          ? Center(
-        child: const CircularProgressIndicator(),
-      )
-          : Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              controller: _controller,
-              itemCount: _posts.length,
-              itemBuilder: (_, index) => Card(
-                margin: const EdgeInsets.symmetric(
-                    vertical: 8, horizontal: 10),
-                child: ListTile(
-                  title: Text(_posts[index]['title']),
-                  subtitle: Text(_posts[index]['body']),
-                ),
-              ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-
-          // when the _loadMore function is running
-          if (_isLoadMoreRunning == true)
-            const Padding(
-              padding: EdgeInsets.only(top: 10, bottom: 40),
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-
-          // When nothing else to load
-          if (_hasNextPage == false)
-            Container(
-              padding: const EdgeInsets.only(top: 30, bottom: 40),
-              color: Colors.amber,
-              child: const Center(
-                child: Text('You have fetched all of the content'),
-              ),
+            ElevatedButton(
+              child: const Text('Blog Page'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const BlogPage(title: "PortPos Blog Page")),
+                );
+              },
             ),
-        ],
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
